9afd10bee944e0fdd5da484153915364ea0345f9
anasnahil <nahil.anas1@gmail.com>
2021-05-17 21:59:24 -0400

    added a working NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import test from './test'
+
 import NavBar from './Components/NavBar'
 
 function App() {
   return (
     <div className="App">
       <h1>Fauxtube</h1>
-      <test />
       <NavBar />
     </div>
   );
