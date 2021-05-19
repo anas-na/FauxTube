7a35fb2bc63d3d73e979933d3a4dc9cd813a06f0
Shoaib Dar <SDAR30@gmail.com>
2021-05-18 21:29:00 -0400

    api call video retrevial

--- src/Components/YoutubeAPICalls.js
+++ src/Components/YoutubeAPICalls.js
@@ -3,17 +3,24 @@ import { useEffect, useState } from "react";
 
 const YoutubeAPICalls = () => {
     const [test, setTest] = useState();
+    const [videos, setVideos] = useState([])
 
     const getYouTube = async () => {
-        const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY }`);
-        console.log(res);
+        let input = "programming"
+        const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${input}&type=video&key=${process.env.REACT_APP_API_KEY}`);
+        setVideos(res.data.items)
+        console.log(res.data.items)
     }
 
     useEffect(() => {
         getYouTube();
     }, [])
 
-    return(null);
+    return (<section>
+        {videos.map(video => <iframe width="420" height="315"
+            src={`https://www.youtube.com/embed/${video.id.videoId}`}>
+        </iframe>)}
+    </section>);
 }
 
 export default YoutubeAPICalls;
