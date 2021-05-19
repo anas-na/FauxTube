9d615f17fe963b89bb428999103f30ac3d3ef186
anasnahil <nahil.anas1@gmail.com>
2021-05-18 21:10:20 -0400

    innitialized HomePage

---
+++ src/Components/HomePage.js
@@ -0,0 +1,11 @@
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Search for Videos</h1>
+            <input value='userInput' placeholder='search for video'/>
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default HomePage
