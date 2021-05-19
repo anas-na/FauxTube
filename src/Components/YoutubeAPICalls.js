bdc355f1ad64b4ef80cfe8fbd88142f47ab58aa0
joshuamendez <joshuafmendez@gmail.com>
2021-05-18 20:43:25 -0400

    added function to test youtubeAPI calls

--- src/Components/YoutubeAPICalls.js
+++ src/Components/YoutubeAPICalls.js
@@ -1,9 +1,16 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const YoutubeAPICalls = () => {
     const [test, setTest] = useState();
 
+    const getYouTube = async () => {
+        const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY }`);
+        console.log(res);
+    }
+
     useEffect(() => {
+        getYouTube();
     }, [])
 
     return(null);
