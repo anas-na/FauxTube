c82d98bd083fbe5aaee8c198046820025b6bfaa3
anasnahil <nahil.anas1@gmail.com>
2021-05-19 20:50:58 -0400

    added bio

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -1,10 +1,12 @@
+import './AboutPage.css'
+
 const AboutPage = () => {
     return (<section className="aboutPageContainer">
         <h1>FauxTube</h1>
         <p>An application that allows you to search for videos on YouTube while using a simple custom user interface.</p>
         {/* Alphabetical Order */}
         <h3>Anas Nahil</h3>
-        <p>bio</p>
+        <p>With a passion for Video Games and Music, I hope to use the skills i'm learning as a web developer to pursue a tech career in one of those fields.</p>
         <a href="https://github.com/itsanasna" target='_blank'>GitHub</a>
         <h3>Joshua Mendez</h3>
         <p>bio</p>
