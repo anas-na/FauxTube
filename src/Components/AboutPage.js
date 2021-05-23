21801db464a7d58465e77e4407167fc073dc1a73
Shoaib Dar <SDAR30@gmail.com>
2021-05-23 15:27:52 -0400

    Homepage (#31)
    * fixed homepage errors
    
    * style search form in home
    
    * added order of videos select bar
    
    * styled homepage grid
    
    * Shoaib bio and styling

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -25,44 +25,44 @@ const AboutPage = () => {
         {showBio ? (
             <div className='BioSection'>
                 <div class="bioImage-wrapper">
-                <img src='https://media-exp1.licdn.com/dms/image/C4D03AQFc0VAb9nmoWA/profile-displayphoto-shrink_800_800/0/1606956485295?e=1627516800&v=beta&t=51sgPlWH0eYNmXUBAF22IL3ItOq7_LLwLn2qtFP6V90' class="CollabImage" alt='collaborator'/>
-                <a href="https://github.com/itsanasna" target='_blank' rel="noreferrer" className='GitHub'>GitHub</a>
-                <a href="https://www.linkedin.com/in/itsanasna/" target='_blank' rel="noreferrer" className='linkedIn'>LinkedIn</a>
+                    <img src='https://media-exp1.licdn.com/dms/image/C4D03AQFc0VAb9nmoWA/profile-displayphoto-shrink_800_800/0/1606956485295?e=1627516800&v=beta&t=51sgPlWH0eYNmXUBAF22IL3ItOq7_LLwLn2qtFP6V90' class="CollabImage" alt='collaborator' />
+                    <a href="https://github.com/itsanasna" target='_blank' rel="noreferrer" className='GitHub'>GitHub</a>
+                    <a href="https://www.linkedin.com/in/itsanasna/" target='_blank' rel="noreferrer" className='linkedIn'>LinkedIn</a>
                 </div>
                 <p>As a programmer, I love being able to expand my horizons and see what's possible. My curiosity and drive for perfection means that I excel at solving technical challenges and debugging individual lines of code. With a passion for Video Games and Music, I hope to use the skills i'm learning as a web developer to pursue a tech career in one of those fields.</p>
             </div>
         ) : (
             null
-            )}            
+        )}
         {/* <h3>Joshua Mendez</h3> */}
         <h3 onClick={handleClick1}>{showBio1 ? "Joshua Mendez" : "Joshua Mendez"}</h3>
         {showBio1 ? (
             <div className='BioSection'>
                 <div class="bioImage-wrapper">
-                <img src='https://media-exp1.licdn.com/dms/image/C4D03AQEesClN2khnMA/profile-displayphoto-shrink_800_800/0/1608595537276?e=1627516800&v=beta&t=28m6mM_oC38oCCENaKseUU-RjwPPSSdnNobEzgVNAXE' class="CollabImage" alt='collaborator'/>
-                <a href="https://github.com/joshuafmendez" target='_blank' rel="noreferrer" className='GitHub'>GitHub</a>
-                <a href="https://www.linkedin.com/in/joshuafmendez/" target='_blank' rel="noreferrer" className='linkedIn'>LinkedIn</a>
+                    <img src='https://media-exp1.licdn.com/dms/image/C4D03AQEesClN2khnMA/profile-displayphoto-shrink_800_800/0/1608595537276?e=1627516800&v=beta&t=28m6mM_oC38oCCENaKseUU-RjwPPSSdnNobEzgVNAXE' class="CollabImage" alt='collaborator' />
+                    <a href="https://github.com/joshuafmendez" target='_blank' rel="noreferrer" className='GitHub'>GitHub</a>
+                    <a href="https://www.linkedin.com/in/joshuafmendez/" target='_blank' rel="noreferrer" className='linkedIn'>LinkedIn</a>
                 </div>
                 <p>I have had a passion for programming since a young age as an avid gamer. Hoping to use my skills I learn when trying to make mods when I was younger and now with formal learning with Pursuit aming for a career as a software developer</p>
             </div>
         ) : (
             null
-            )}
+        )}
         {/* <h3>Shoaib Dar</h3> */}
         <h3 onClick={handleClick2}>{showBio2 ? "Shoaib Dar" : "Shoaib Dar"}</h3>
         {showBio2 ? (
             <div className='BioSection'>
                 <div class="bioImage-wrapper">
-                <img src='https://media-exp1.licdn.com/dms/image/C4E03AQEs1mpVIgFwBw/profile-displayphoto-shrink_800_800/0/1606586088168?e=1627516800&v=beta&t=F8puEkH0pA_77_HQJMdeAtWhL3B7SJJwgnxWXtLayOc' class="CollabImage" alt='collaborator'/>
-                <a href="https://github.com/SDAR30" target='_blank' rel="noreferrer" className='GitHub'>GitHub</a>
-                <a href="https://www.linkedin.com/in/shoaib-dar/" target='_blank' rel="noreferrer" className='linkedIn'>LinkedIn</a>
+                    <img src='https://media-exp1.licdn.com/dms/image/C4E03AQEs1mpVIgFwBw/profile-displayphoto-shrink_800_800/0/1606586088168?e=1627516800&v=beta&t=F8puEkH0pA_77_HQJMdeAtWhL3B7SJJwgnxWXtLayOc' class="CollabImage" alt='collaborator' />
+                    <a href="https://github.com/SDAR30" target='_blank' rel="noreferrer" className='GitHub'>GitHub</a>
+                    <a href="https://www.linkedin.com/in/shoaib-dar/" target='_blank' rel="noreferrer" className='linkedIn'>LinkedIn</a>
                 </div>
-                <p>I’m a analytical Full Stack Developer who loves to solve problems and puzzles. I enjoy traveling, reading and learning about the world we live in.</p>
-                
+                <p>I’m a analytical Full Stack Developer who loves to solve problems and puzzles. I enjoy reading, traveling, and learning about the world we live in, and have open-minded conversations with the people in it. Growing up, I have been helped immensely by my community through difficult times and I can't wait to start my career so I can repay back my community one day.</p>
+
             </div>
         ) : (
             null
-            )}
+        )}
     </section>)
 }
 
