a3dba15886934fab05b6dc1af3fe3b6b4e18c917
anasnahil <nahil.anas1@gmail.com>
2021-05-23 13:35:56 -0400

    about page css

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -20,11 +20,12 @@ const AboutPage = () => {
         <h1>FauxTube</h1>
         <p>An application that allows you to search for videos on YouTube while using a simple custom user interface.</p>
         {/* Alphabetical Order */}
-        <h2 className='collabs'>The FauxTube Creators:</h2>
+        <h2 className='collabs'>Collaborators:</h2>
         <h3 onClick={handleClick}>{showBio ? "Anas Nahil" : "Anas Nahil"}</h3>
         {showBio ? (
-            <div>
-                <p>With a passion for Video Games and Music, I hope to use the skills i'm learning as a web developer to pursue a tech career in one of those fields.</p>
+            <div className='BioSection'>
+                <img src='https://media-exp1.licdn.com/dms/image/C4D03AQFc0VAb9nmoWA/profile-displayphoto-shrink_800_800/0/1606956485295?e=1627516800&v=beta&t=51sgPlWH0eYNmXUBAF22IL3ItOq7_LLwLn2qtFP6V90'/>
+                <p>As a programmer, I love being able to expand my horizons and see what's possible. My curiosity and drive for perfection means that I excel at solving technical challenges and debugging individual lines of code. With a passion for Video Games and Music, I hope to use the skills i'm learning as a web developer to pursue a tech career in one of those fields.</p>
                 <a href="https://github.com/itsanasna" target='_blank' rel="noreferrer">GitHub</a>
             </div>
         ) : (
@@ -33,7 +34,8 @@ const AboutPage = () => {
         {/* <h3>Joshua Mendez</h3> */}
         <h3 onClick={handleClick1}>{showBio1 ? "Joshua Mendez" : "Joshua Mendez"}</h3>
         {showBio1 ? (
-            <div>
+            <div className='BioSection'>
+                <img src='https://media-exp1.licdn.com/dms/image/C4D03AQEesClN2khnMA/profile-displayphoto-shrink_800_800/0/1608595537276?e=1627516800&v=beta&t=28m6mM_oC38oCCENaKseUU-RjwPPSSdnNobEzgVNAXE'/>
                 <p>I have had a passion for programming since a young age as an avid gamer. Hoping to use my skills I learn when trying to make mods when I was younger and now with formal learning with Pursuit aming for a career as a software developer</p>
                 <a href="https://github.com/joshuafmendez" target='_blank' rel="noreferrer">GitHub</a>
             </div>
@@ -43,7 +45,8 @@ const AboutPage = () => {
         {/* <h3>Shoaib Dar</h3> */}
         <h3 onClick={handleClick2}>{showBio2 ? "Shoaib Dar" : "Shoaib Dar"}</h3>
         {showBio2 ? (
-            <div>
+            <div className='BioSection'>
+                <img src='https://media-exp1.licdn.com/dms/image/C4E03AQEs1mpVIgFwBw/profile-displayphoto-shrink_800_800/0/1606586088168?e=1627516800&v=beta&t=F8puEkH0pA_77_HQJMdeAtWhL3B7SJJwgnxWXtLayOc'/>
                 <p>I’m a analytical Full Stack Developer who loves to solve problems and puzzles. I enjoy traveling, reading and learning about the world we live in.</p>
                 <a href="https://github.com/SDAR30" target='_blank' rel="noreferrer">GitHub</a>
                 
--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import './Video.css'
+import './HomePage.css'
 import moment from 'moment';
 
 const Video = (props) =>{
@@ -24,7 +25,7 @@ const Video = (props) =>{
     return (
         <section>
             <br/>
-            <button onClick={props.history.goBack}>Go Back</button>
+            <button onClick={props.history.goBack} className="search-button">Go Back</button>
             <br/>
             {<iframe title='youtubeVideo' width="420" height="315"
             src={`https://www.youtube.com/embed/${props.match.params.id}`}>
@@ -38,7 +39,7 @@ const Video = (props) =>{
                     Comment:
                     <textarea rows="4" cols="40" value={comment} onChange={handleComment} required></textarea>
                 </label>
-                <button type='submit'>Submit</button>
+                <button type='submit' className='search-button'>Submit</button>
             </form>
             {commentList.map(commentObj => {
                 return (<div key={props.match.params.id} className='commentContainer'>
