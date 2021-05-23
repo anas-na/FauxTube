efc65ec08d13bb2280cf557310c6ae3b92741152
joshuamendez <joshuafmendez@gmail.com>
2021-05-23 11:27:24 -0400

    implemented a research for previous search

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,20 +1,34 @@
 import axios from "axios"
 import { Link } from "react-router-dom"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./HomePage.css"
 
-const HomePage = () => {
+const HomePage = (props) => {
     const [videos, setVideos] = useState([]);
     const [input, setInput] = useState('');
-
     const getYouTube = async () => {
-        try {
-            const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${input}&type=video&key=${process.env.REACT_APP_API_KEY}`);
-            setVideos(res.data.items)
-        } catch (error) {
-            console.log(error)
+        if(!props.search){
+            try {
+                const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${input}&type=video&key=${process.env.REACT_APP_API_KEY}`);
+                props.setSearch(res.config.url);
+                setVideos(res.data.items);
+            } catch (error) {
+                console.log(error)
+            }
         }
     }
+
+    useEffect(()=>{
+        const reload = async () => {
+            try {
+                const res = await axios.get(`${props.search}`);
+                setVideos(res.data.items);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        reload();
+    },[props.search])
 
     const selectOrder = async (e) => {
         const order = e.target.value;
